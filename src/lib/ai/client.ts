73ae909a903b0fe83a6ec70c6693cbbd305d994d
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-08-26 18:16:08 +0530

    feat: Fetch api for model streaming

---
+++ src/lib/ai/client.ts
@@ -0,0 +1,37 @@
+import { toUIMessageStream } from "@ai-sdk/langchain";
+import { createUIMessageStreamResponse, type UIMessage } from "ai";
+import { ChatOllama } from "@langchain/ollama";
+import { AIMessage, HumanMessage } from "@langchain/core/messages";
+
+export const FetchStreamingResponse = async (
+  _input: RequestInfo | URL,
+  init?: RequestInit,
+) => {
+  const { messages }: { messages: UIMessage[] } = JSON.parse(
+    init?.body as string,
+  );
+
+  const model = new ChatOllama({
+    model: "llama3.2",
+  });
+
+  const stream = await model.stream(
+    messages.map((message) =>
+      message.role == "user"
+        ? new HumanMessage(
+            message.parts
+              .map((part) => (part.type === "text" ? part.text : ""))
+              .join(""),
+          )
+        : new AIMessage(
+            message.parts
+              .map((part) => (part.type === "text" ? part.text : ""))
+              .join(""),
+          ),
+    ),
+  );
+
+  return createUIMessageStreamResponse({
+    stream: toUIMessageStream(stream),
+  });
+};
